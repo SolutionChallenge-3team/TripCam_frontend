9dbbf08782c39f75ad3ca2098f8a5d3ff82a5088
s-duswn <tlsduswnim@gmail.com>
2025-05-06 01:02:45 +0900

    feat: 닉네임 변경 버튼 추가

--- lib/common/button.dart
+++ lib/common/button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tripcam/common/theme.dart';
 
 class CommonBackButton extends StatelessWidget {
   final Color color;
@@ -17,6 +18,34 @@ class CommonBackButton extends StatelessWidget {
       color: color,
       iconSize: 20,
       onPressed: onPressed ?? () => Navigator.pop(context),
+    );
+  }
+}
+
+class NicknameButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onTap;
+
+  const NicknameButton({super.key, required this.text, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.sub,
+        padding: const EdgeInsets.symmetric(vertical: 12),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(
+          fontFamily: 'Pretendard',
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+          color: Colors.white,
+        ),
+      ),
     );
   }
 }
