285881f49b5c77e5021f075b3a4e0766f4037d2d
s-duswn <tlsduswnim@gmail.com>
2025-05-13 22:48:18 +0900

    feat: 메인 래퍼 및 전체 라우팅 설정 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:tripcam/settings/logout_setting.dart';
-import 'package:tripcam/settings/settinig_list.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:tripcam/auth/login.dart';
+import 'package:tripcam/main_wrapper.dart';
+
+import 'firebase_options.dart';
+import 'camera/camera_screen.dart';
+import 'loding/loading_screen.dart';
+import 'settings/logout_setting.dart';
+import 'settings/settinig_list.dart';
+import 'recomend/recommend_screen.dart';
+import 'History/history_screen.dart';
 import 'common/theme.dart';
 import 'settings/nickname_setting.dart';
 import 'home.dart';
+import 'calendar/calendar_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Firebase 초기화
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // 날짜 형식 로케일 초기화
+  await initializeDateFormatting('ko_KR', null);
+
   runApp(const MyApp());
 }
 
@@ -18,8 +37,14 @@ class MyApp extends StatelessWidget {
       title: 'TripCam',
       debugShowCheckedModeBanner: false,
       theme: Appfonts.lightTheme,
-      home: const SettingsList(),
-      routes: {'/nickname': (context) => const NicknameSetting()},
+      home: const MainWrapper(),
+      routes: {
+        '/nickname': (context) => const NicknameSetting(),
+        '/camera': (context) => const CameraScreen(),
+        '/recommend': (context) => const RecommendScreen(),
+        '/history': (context) => const HistoryScreen(imageFile: null),
+        '/loading': (context) => const LoadingScreen(),
+      },
     );
   }
 }
